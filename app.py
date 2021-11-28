8d1ddd91c007dcc2a1d1a53329d0a806c23c284e
PacificPR <prashant.kumarjha63@gmail.com>
2021-11-28 22:52:49 +0530

    Added support for more than two captions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask , render_template , redirect , url_for , request , send_file
 import requests
 import shutil
+import json
 
 
 app = Flask(__name__)
@@ -24,7 +25,8 @@ def create():
     if request.method == "POST":
         m_url = request.form.get('meme_url')
         m_id =  request.form.get('meme_id')
-        return render_template("create.html",img=m_url,id=m_id)
+        m_box_count = int(request.form.get('meme_box_count'))
+        return render_template("create.html",img=m_url,id=m_id,box_count=m_box_count)
     else:
         return redirect(url_for(search))
 
@@ -32,17 +34,22 @@ def create():
 def final():
     if request.method == "POST":
         URL = "https://api.imgflip.com/caption_image"
-        text_top = request.form.get('top')
-        text_bot = request.form.get('bot')
-        id = request.form.get('temp_id')
-    
-        PARAMS = {'template_id':id,'username':'pacificPR','password':'thisis1secret','text0':text_top,'text1':text_bot}
-        data = requests.post(url=URL,params=PARAMS)
-        fin = data.json() 
 
+        id = request.form.get('temp_id')
+        box_count = int(request.form.get('box_count'))
+        
+        PARAMS = {'template_id':id,'username':'pacificPR','password':'thisis1secret'}
+        
+        for i in range(box_count):
+            PARAMS[f"boxes[{i}][text]"]=request.form.get(str(i))
+
+        data = requests.post(url=URL,data=PARAMS)
+
+        fin = data.json() 
         if fin["success"]:
             return render_template("final.html",img=fin["data"]["url"])
         else:
+            print(fin["error_message"])
             return "Error Loading Request, Retry ..."
     else:
         return redirect(url_for("search"))
